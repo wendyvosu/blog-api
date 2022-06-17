e49c3316aff52b3d39f76e3f80b03eb86a4e0cfd
Wendy Vuong <vuongw@oregonstate.edu>
2022-06-17 09:42:22 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const usersRouter = require("./routes/usersRouter")
 const authRouter = require("./routes/authRouter")
 
 const app = express()
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 
 app.use(express.json())
 app.use(morgan("dev"))
